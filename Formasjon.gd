59a406c271e6456e77e36eb8e376623e8854e66a
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-05-23 18:37:24 +0200

    etter siste gang

--- Formasjon.gd
+++ Formasjon.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 export var speed = 100
 
-var target_postion
+var target_position
 var left_bound
 var right_bound
 var is_in_position = false
+var direction
 
 func _ready():
-	target_position = rand_raange(200, get_viewport_rect().size.y / 2)
+	target_position = rand_range(200, get_viewport_rect().size.y / 2)
 	left_bound = position.x - 100
 	right_bound = position.x + 100
 	direction = 1 if rand_range(0,100) > 50 else -1
