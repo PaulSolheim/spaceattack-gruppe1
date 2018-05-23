c44b7caacf9f76aa344df62398811a999ca36dc7
Pål Grønstad Solheim <paul.solheim@gmail.com>
2018-05-23 15:18:52 +0200

    Før siste gang
    Endringer til siste kodekveld.

--- Spiller.gd
+++ Spiller.gd
@@ -1,11 +1,12 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 export(PackedScene) var projectile
+
+onready var timer = $Timer
+
 var speed = 500
 var screen_size
+var can_shoot = true
 
 func _ready():
 	screen_size = get_viewport_rect().size.x
@@ -17,6 +18,14 @@ func _process(delta):
 		position.x -= speed * delta
 	elif Input.is_action_pressed("right"):
 		position.x += speed * delta
-	if Input.is_action_pressed("shoot"):
+	if  can_shoot and Input.is_action_pressed("shoot"):
+		can_shoot = false
 		var new_projectile = projectile.instance()
-		add_child(new_projectile)
+		get_parent().add_child(new_projectile)
+		new_projectile.position = position
+		timer.start()
+
+
+func _on_Timer_timeout():
+	can_shoot = true
+	
